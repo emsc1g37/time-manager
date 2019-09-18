cee15dd4ca1398ac302f38da56c88e21b286a340
maelalle@outlook.fr <maelalle@outlook.fr>
2019-09-18 09:49:30 +0200

    refactor

--- services/usersService.js
+++ services/usersService.js
@@ -2,113 +2,37 @@ const db = require("../db/config");
 const shared = require("./shared");
 
 function getUserById(userId) {
-  return new Promise((resolve, reject) => {
-    const sql = "SELECT * FROM users WHERE id = $1";
-    shared
-      .execute(sql, [userId])
-      .then(results => {
-        console.log(results);
-        resolve(results);
-      })
-      .catch(err => {
-        console.log(err);
-        reject(err);
-      });
-  }).catch(error => {
-    return error;
-  });
+  return shared.execute("SELECT * FROM users WHERE id = $1", [userId]);
 }
 
 function getAllUsers() {
-  return new Promise((resolve, reject) => {
-    const sql = "SELECT * FROM users";
-    shared
-      .execute(sql, [])
-      .then(results => {
-        console.log(results);
-        resolve(results);
-      })
-      .catch(err => {
-        console.log(err);
-        reject(err);
-      });
-  }).catch(error => {
-    return error;
-  });
+  return shared.execute("SELECT * FROM userss", []);
 }
 
 function createUser(email, username) {
-  return new Promise((resolve, reject) => {
-    const sql = "INSERT INTO users (email, username) VALUES ($1, $2)";
-    shared
-      .execute(sql, [email, username])
-      .then(results => {
-        console.log(results);
-        resolve(results);
-      })
-      .catch(err => {
-        console.log(err);
-        reject(err);
-      });
-  }).catch(error => {
-    return error;
-  });
+  return shared.execute("INSERT INTO users (email, username) VALUES ($1, $2)", [
+    email,
+    username
+  ]);
 }
 
 function updateUser(email, username) {
-  return new Promise((resolve, reject) => {
-    const sql = "UPDATE users SET email = $1, username = $2";
-    shared
-      .execute(sql, [email, username])
-      .then(results => {
-        console.log(results);
-        resolve(results);
-      })
-      .catch(err => {
-        console.log(err);
-        reject(err);
-      });
-  }).catch(error => {
-    return error;
-  });
+  return shared.execute("UPDATE users SET email = $1, username = $2", [
+    email,
+    username
+  ]);
 }
 
 function deleteUser(userId) {
-  return new Promise((resolve, reject) => {
-    const sql = "DELETE FROM users WHERE id = $1";
-    shared
-      .execute(sql, [userId])
-      .then(results => {
-        console.log(results);
-        resolve(results);
-      })
-      .catch(err => {
-        console.log(err);
-        reject(err);
-      });
-  }).catch(error => {
-    return error;
-  });
+  return shared.execute("DELETE FROM users WHERE id = $1", [userId]);
 }
 
 function getUserByEmailAndUsername(email, username) {
-  return new Promise((resolve, reject) => {
-    const sql =
-      "SELECT * FROM users WHERE email LIKE LOWER($1) " +
-      "AND username LIKE LOWER($2)";
-    shared
-      .execute(sql, [email, username])
-      .then(results => {
-        resolve(results);
-      })
-      .catch(err => {
-        console.log(err);
-        reject(err);
-      });
-  }).catch(error => {
-    console.log(error);
-    return error;
-  });
+  return shared.execute(
+    "SELECT * FROM users WHERE email LIKE LOWER($1) " +
+      "AND username LIKE LOWER($2)",
+    [email, username]
+  );
 }
 
 module.exports = {
