07c30fd52abf9aca389133a614a50b8cc5f89e8f
DataTriny <datatriny@gmail.com>
2019-09-30 16:26:04 +0200

    Fix Clock in route

--- api/routes/workingTimesController.js
+++ api/routes/workingTimesController.js
@@ -4,16 +4,20 @@ async function clockInAndOut(req, res) {
   const result = await workingTimesService.getLastFor(req.user.id);
   if (result.error)
     res.status(500).json(result).end();
-  else if (result.data.length == 0 || result.data[0].departure === undefined) {
-    const result = await workingTimesService.clockIn(req.user.id);
-    if (result.error)
-      res.status(500).json(result).end();
-    else
-      res.status(201).json({
-        user_id: req.user.id,
-        id: result.data[0].id,
-        arrival: result.data[0].arrival
-      }).end();
+  else if (result.data.length == 0 || result.data[0].departure !== undefined) {
+    if (isToday(result.data[0].departure))
+      res.status(400).json({error: "You can only clock in once per day."}).end();
+    else {
+      const result = await workingTimesService.clockIn(req.user.id);
+      if (result.error)
+        res.status(500).json(result).end();
+      else
+        res.status(201).json({
+          user_id: req.user.id,
+          id: result.data[0].id,
+          arrival: result.data[0].arrival
+        }).end();
+    }
   }
   else {
     const result = await workingTimesService.clockOut(req.user.id);
@@ -38,6 +42,13 @@ async function getAllBetween(req, res) {
     res.status(500).json(result).end();
   else
     res.json(result.data);
+}
+
+const isToday = (someDate) => {
+  const today = new Date()
+  return someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
 }
 
 module.exports = {
